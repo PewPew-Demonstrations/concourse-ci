3a5c4b72fb3da9e28948125fb4820bc9b25d1799
Anthony Sceresini <asceresini@gmail.com>
2016-09-07 20:14:51 +1000

    Adding maven repo bucket

--- main.tf
+++ main.tf
@@ -170,3 +170,57 @@ EOF
     EnvironmentId = "${module.stack.environment_id}"
   }
 }
+
+resource "aws_s3_bucket" "maven_repo_logs" {
+  bucket = "${var.team}-${var.role}-${var.name}-maven-repo-logs"
+  acl = "log-delivery-write"
+
+	tags {
+		Name = "${var.name}-maven-repo-logs"
+		Project = "${var.name}"
+		Team = "${var.team}"
+		Owner = "${var.owner}"
+		Environment = "${var.environment}"
+		EnvironmentId = "${module.stack.environment_id}"
+	}
+}
+
+resource "aws_s3_bucket" "maven_repo" {
+  bucket = "${var.team}-${var.role}-${var.name}-maven-repo"
+  acl = "private"
+	logging {
+		target_bucket = "${aws_s3_bucket.maven_repo_logs.id}"
+		target_prefix = "log/"
+	}
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject",
+        "s3:PutObjectAcl",
+        "s3:GetObject",
+        "s3:ListBucket"
+      ],
+      "Principal": {"AWS": [
+        "${module.stack.ecs_worker_role_arn}"
+      ]},
+      "Resource": [
+        "arn:aws:s3:::${var.team}-${var.role}-${var.name}-maven-repo",
+        "arn:aws:s3:::${var.team}-${var.role}-${var.name}-maven-repo/*"
+      ]
+    }
+  ]
+}
+EOF
+  tags {
+    Name = "${var.name}"
+    Project = "${var.name}"
+    Team = "${var.team}"
+    Owner = "${var.owner}"
+    Environment = "${var.environment}"
+    EnvironmentId = "${module.stack.environment_id}"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,4 @@ output "ci_user_arn" { value = "${aws_iam_user.ci.arn}" }
 output "github_app_id" { value = "${var.github_app_id}" }
 output "github_app_secret" { value = "${var.github_app_secret}" }
 output "kms_key_id" { value = "${aws_kms_key.ci.id}" }
+output "maven_repo_bucket" { value = "${aws_s3_bucket.maven_repo.id}" }
